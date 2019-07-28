252ac606f325ab0f1ffe5d305818b1f0eebdb084
Keenan Alves <keenan.alves@gmail.com>
2019-07-28 00:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function theBeatlesPlay (musicians, instruments) {
   
   for(let i = 0; i < musicians.length; i++) {
   play.push(musicians[i] + " plays " + instruments[i]);
-  return musicians
   } 
 }
 
