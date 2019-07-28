a7e81a1e7e56fd6559bd038b5c78e65e750fff77
Keenan Alves <keenan.alves@gmail.com>
2019-07-28 00:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function theBeatlesPlay (musicians, instruments) { 
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   const play[];
+  
   for(let i = 0; i < musicians.length; i++) {
   console.log()
   } 
